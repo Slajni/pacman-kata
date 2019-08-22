ac5b6d346f520f04584e1a0f4e34ca07c38c840e
Slajni <c.damiankucharski@gmail.com>
2019-08-22 19:09:38 +0200

    initia;

--- src/pacman-core/PacMan.ts
+++ src/pacman-core/PacMan.ts
@@ -1,7 +1,66 @@
-class PacMan {
-    public whatAmILike(): string {
-        return 'funny';
+import { BooleanLiteral } from "@babel/types";
+
+export default class PacMan {
+
+    lives: number;
+    points: number;
+    level : number;
+    ballCount : number;
+    ghostCount : object;
+    state: boolean;
+    superTime : number;
+    constructor({lives=3, points=0, level=0, ballCount=0, ghostCount={}, state=false, superTime=0}){
+
+        this.lives = lives;
+        this.points = points;
+        this.level = level;
+        this.ballCount = ballCount;
+        this.state = state;
+        this.superTime = superTime;
+        this.ghostCount = ghostCount;
     }
+
+    tick(){
+        
+        if (this.state){
+            if (this.superTime > 1){
+                this.superTime -= 1;
+            }else{
+                this.superTime = 0;
+                this.state = false;
+            }
+        }
+    }    
+
+    eatBall(ball){
+        this.points += 1;
+        if (this.ballCount >= 39){
+            this.ballCount = 0;
+            this.level += 1;
+        }
+        else
+            this.ballCount += 1;
+        if (ball){
+            this.state = true;
+            this.superTime = 10;
+        }
+
+    }
+
+    eatGhost(name){
+        if (this.state){
+            this.points  += 10;
+            this.ghostCount['total'] += 1;
+            this.ghostCount[name] += 1;
+
+        }else{
+            this.lives -= 1;
+            if (this.lives == 0){
+                this.points = 0;
+            }
+        }
+    }
+
 }
 
-export default PacMan;
+
--- test/PacMan.test.ts
+++ test/PacMan.test.ts
@@ -1,6 +1,41 @@
 import PacMan from '../src/pacman-core/PacMan';
 
-test('basic', () => {
-    const pacman = new PacMan();
-    expect(pacman.whatAmILike()).toBe('funny');
+
+test('tick pacman when is in super state', () => {
+    const pacman = new PacMan({ state: true, superTime: 200 });
+    pacman.tick();
+    expect(pacman.state).toBe(true);
+    expect(pacman.superTime).toBe(199);
 });
+test('tick pacman when state super time one', () => {
+    const pacman = new PacMan({ state: true, superTime: 1 });
+    pacman.tick();
+    expect(pacman.state).toBe(false);
+    expect(pacman.superTime).toBe(0);
+});
+test('tick pacman when state normal time zero', () => {
+    const pacman = new PacMan({ state: false, superTime: 0 });
+    pacman.tick();
+    expect(pacman.state).toBe(false);
+    expect(pacman.superTime).toBe(0);
+});
+
+
+test('eat ball when balls < 40', () => {
+
+    const pacman = new PacMan({ballCount:20});
+    pacman.eatBall(false);
+    expect(pacman.ballCount).toBe(21);
+
+})
+
+test('eat ball when balls > 40', () => {
+
+    const pacman = new PacMan({ballCount:41, level: 1});
+    let current_level = pacman.level;
+    pacman.eatBall(false);
+    expect(pacman.ballCount).toBe(0);
+    expect(pacman.level).toBe(2);
+    
+
+})
